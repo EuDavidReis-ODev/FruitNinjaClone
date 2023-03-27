9d04b9cbc6604466e60d26827ee28c35c3e710ee
EuDavidReis-ODev <eudavidreis.dev@gmail.com>
2023-03-27 10:56:28 -0300

    Background configurado.

--- Assets/_Game/Scripts/Background.cs
+++ Assets/_Game/Scripts/Background.cs
@@ -7,12 +7,21 @@ public class Background : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer sprite = GetComponentInChildren<SpriteRenderer>();
+
+        Vector3 scaleTemp = GetComponentInChildren<Transform>().transform.localScale;
+
+        float width = sprite.bounds.size.x;
+        float height = sprite.bounds.size.y;
+
+        float heightCamera = Camera.main.orthographicSize * 2.0f;
+        float widthWorld = heightCamera / Screen.height * Screen.width;
+
+        scaleTemp.x = widthWorld / width;
+        scaleTemp.y = heightCamera / height;
+
+        transform.localScale = scaleTemp; 
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
