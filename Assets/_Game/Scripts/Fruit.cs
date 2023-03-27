da906917a0ad11004e91c48213851d12d31df0bb
EuDavidReis-ODev <eudavidreis.dev@gmail.com>
2023-03-27 12:10:11 -0300

    Impulsionando as frutas.

---
+++ Assets/_Game/Scripts/Fruit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+
+    [SerializeField] private float startForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = this.gameObject.GetComponent<Rigidbody2D>();
+        ApplyForce();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ApplyForce(){
+        myRB.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+
+    }
+}
