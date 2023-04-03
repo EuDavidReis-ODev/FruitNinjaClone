971304a12e1ef94d49c0296e4890c4ca5aea772c
EuDavidReis-ODev <eudavidreis.dev@gmail.com>
2023-04-03 15:53:12 -0300

    Programação da interface de usuário.

--- Assets/_Game/Scripts/GameController.cs
+++ Assets/_Game/Scripts/GameController.cs
@@ -11,15 +11,25 @@ public class GameController : MonoBehaviour
     [HideInInspector] public Color32 coconutColor = new Color32(134,87,56,255);
     [HideInInspector] public Color32 pineappleColor = new Color32(202,111,26,255);
     [HideInInspector] public Color32 orangeColor = new Color32(253,122,10,255);
+    
+    private UIController uIController;
+
+    [HideInInspector] public int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uIController = FindAnyObjectByType<UIController>();
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame(){
+        uIController.txtScore.text = "Score: "+ score;
     }
 }
